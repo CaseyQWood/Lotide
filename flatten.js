4299279c0c0534442e806332e61a9b3020c95f46
Casey Wood <caseywood252@gmail.com>
2021-05-26 06:19:57 +0000

    adding flattan array function

---
+++ flatten.js
@@ -0,0 +1,17 @@
+const flatten = function(input){
+  let final = [];
+  for (var index of input) {
+    if(Array.isArray(index)) {
+      for( var dex of index) {
+        final.push(dex)
+      }
+    }
+    else { 
+      final.push(index )
+    }
+  }
+  console.log(final)
+}
+
+
+flatten([1, 2, [3, 4], 5, [6]])
