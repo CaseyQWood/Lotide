feeb9cc5f41fa63482c0aa6ed479576931e03f5b
Casey Wood <caseywood252@gmail.com>
2021-05-26 01:39:29 +0000

    refactored and updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,23 @@
 const eqArrays = function(x, y) {
-  let finalAsnwer = '';
-  if (x.length === y.length) {
-    for (let i = 0; i < x.length; i++) {
-      if (x[i] === y[i]) {
-        finalAsnwer = true;
-      } else {
-        finalAsnwer = false;
-      }
-    }
-  } else {
-    finalAsnwer = false;
+  if (x.length !== y.length) {
+    return false
   }
-  return finalAsnwer;
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== y[i]) {
+      return false
+    } 
+  }
+  return true;
 };
 
 
-const assertEqualArray = function(actual, expected) {
-  if (actual === expected) {
-    return(`Assertion Passed: ${actual} === ${expected}`);
+const assertEqualArray = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed: ${array1} === ${array2}`);
   } else {
-    return(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
-console.log(assertEqualArray(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqualArray(eqArrays([1, 2, 3], [3, 2, 1]), false));
+assertEqualArray([1, 2, 3], [1, 2, 3]);
+assertEqualArray([1, 2, 3], [3, 2, 1]);
