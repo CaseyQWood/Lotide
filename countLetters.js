a11ec0867a6a57fda4abcb4dea703a1209c1c198
Casey Wood <caseywood252@gmail.com>
2021-05-27 00:17:00 +0000

    update for count letters and count only assignemnts

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(input) {
+  const finalObj = {};
+  for (const index of input) {
+    if(index !== ' '){
+      if(!finalObj[index]){
+        finalObj[index] = 1
+      } else {
+        finalObj[index] += 1
+      }
+    }
+  }
+  console.log(finalObj)
+}
+
+
+assertEqual(countLetters("lighthouse in the house"), {l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1, } )
