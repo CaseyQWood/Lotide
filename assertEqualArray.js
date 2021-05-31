f516db0345856cb1b60475574a1235fc3416fa11
Casey Wood <caseywood252@gmail.com>
2021-05-31 21:20:35 +0000

    refactored for tests, created test file and renamed asserArrayEquals

---
+++ assertEqualArray.js
@@ -0,0 +1,12 @@
+const eqArrays = require('./eqArrays')
+
+
+const assertEqualArray = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+module.exports = assertEqualArray
--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const eqArrays = function(x, y) {
   if (x.length !== y.length) {
     return false
