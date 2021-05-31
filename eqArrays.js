495c54f0b9a86f9a22b65dbf482b4f705df8c575
Casey Wood <caseywood252@gmail.com>
2021-05-31 06:32:15 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -8,28 +8,23 @@ const assertEqual = function(actual, expected) {
 
 
 const eqArrays = function(x, y) {
-  let finalAsnwer = '';
-  if (x.length === y.length) {
+  if (x.length !== y.length) {
+    return false
+  }
     for (let i = 0; i < x.length; i++) {
-      if (x[i] === y[i]) {
-        finalAsnwer = true;
-      }
-      else {
-        finalAsnwer = false;
-      }
+      if (x[i] !== y[i]) {
+        return false;
     }
   }
-  else {finalAsnwer = false ;
-  }
-  console.log(finalAsnwer);
+  return true;
 };
 
 
 // create a function called eqArrays
 //loop through each array and compair each index 
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)// => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
