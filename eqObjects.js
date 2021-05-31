8ee1a504b42de11dceb2c8c91ca6ea1132cdd423
Casey Wood <caseywood252@gmail.com>
2021-05-31 06:57:54 +0000

    refactored and updated for stretch assignment

--- eqObjects.js
+++ eqObjects.js
@@ -9,6 +9,14 @@ const assertEqual = function(actual, expected) {
 };
 
 
+// const eqObjects = (obj1, obj2) => {
+ 
+// }
+
+
+
+
+
 const eqObjects = function(object1, object2) {
   let length1 = Object.keys(object1).length;
   let length2 = Object.keys(object2).length;
@@ -16,12 +24,25 @@ const eqObjects = function(object1, object2) {
   if (length1 !== length2) {
       return false;
   }
+
     for (const key in object2) {
       if (Array.isArray(object1[key]) && Array.isArray(object1[key])) {
-        if ( !eqArrays(object1[key], object2[key]) ) {
+        if (eqArrays(object1[key], object2[key]) === false ) {
           return false;
         }
-      } else if (object1[key] !== object1[key]) {  
+      }
+
+      if (object1[key].constructor !== object2[key].constructor) {
+        return false
+      }
+
+      if (object1[key].constructor === Object && object2[key].constructor === Object) {
+        if (eqObjects(object1[key], object2[key]) === false) {
+          return false;
+        }
+      }
+
+      if (object1[key] !== object1[key]) {  
         return false;
       } 
     }
@@ -29,21 +50,22 @@ const eqObjects = function(object1, object2) {
 };
 
 const eqArrays = function(x, y) {
-  let finalAsnwer = '';
-  if (x.length === y.length) {
+  if (x.length !== y.length) {
+    return false
+  }
     for (let i = 0; i < x.length; i++) {
-      if (x[i] === y[i]) {
-        finalAsnwer = true;
-      }
-      else {
-        finalAsnwer = false;
-      }
+      if (x[i] !== y[i]) {
+        return false;
     }
   }
-  else {finalAsnwer = false ;
-  }
-  return finalAsnwer;
+  return true;
 };
+
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false)
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)
+
 
 const ab = { a: "1", b: "2" };
 const abc = { a: "1", b: "2", c: "3" };
