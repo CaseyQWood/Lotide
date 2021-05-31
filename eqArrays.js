0890e0e954b8fb61356b612af925ea9617c86528
Casey Wood <caseywood252@gmail.com>
2021-05-31 07:04:15 +0000

    refactored for recursion stretch

--- eqArrays.js
+++ eqArrays.js
@@ -12,6 +12,14 @@ const eqArrays = function(x, y) {
     return false
   }
     for (let i = 0; i < x.length; i++) {
+      if (x[i].operator === Array !== x[i].operator === Array) {
+        return false;
+      }
+      if (x[i].operator === Array && x[i].operator === Array) {
+        if(eqArrays(x[i], y[i]) === false) {
+          return false;
+        }
+      }
       if (x[i] !== y[i]) {
         return false;
     }
@@ -24,7 +32,7 @@ const eqArrays = function(x, y) {
 //loop through each array and compair each index 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)// => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [3, [2, 4], 1]), false); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
